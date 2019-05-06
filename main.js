a6c620dc0d44a5a68c2cd5f19f974004e543cbe0
learneeer <learneeer.9@gmail.com>
2019-05-07 00:08:53 +0200

    basic_wersion

---
+++ main.js
@@ -0,0 +1,7 @@
+var open_nav = function() {
+    document.getElementById("sidenav").style.width = "200px";
+}
+
+var close_nav = function() {
+    document.getElementById("sidenav").style.width = "0";
+}
